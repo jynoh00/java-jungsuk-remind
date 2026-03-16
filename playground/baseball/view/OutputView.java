9c29a357470eeca6b718cef2f7a5a009f2982084
jynoh00 <wndus123sh@naver.com>
2026-03-17 04:39:04 +0900

    feat: 에러 및 재시작 메시지 출력 메서드 구현

--- playground/baseball/view/OutputView.java
+++ playground/baseball/view/OutputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.common.ErrorMessage;
+import baseball.common.ViewMessage;
 import baseball.model.Answer;
 import baseball.model.GameResult;
 
@@ -12,5 +14,13 @@ public class OutputView {
 
     public void showResult(GameResult gameResult) {
         //...
+    }
+
+    public void showError(String error) {
+        System.out.println(ErrorMessage.ERROR_DEFAULT.getMessage() + error);
+    }
+
+    public void showRestartMessage() {
+        System.out.println(ViewMessage.RESTART_MESSAGE.getMessage());
     }
 }
